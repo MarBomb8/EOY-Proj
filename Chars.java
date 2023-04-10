e2b19648926a531a99324ad2d62984c7626f8181
MarBomb8 <marlyssa.bombarde@yahoo.com>
2023-04-10 15:05:16 -0500

    Char

--- Chars.java
+++ Chars.java
@@ -1,3 +1,71 @@
+import javax.swing.ImageIcon;
+
 public class Chars {
+    private int x;
+    private int y;
+    private int dx;
+    private int dy;
+    private int w;
+    private int h;
+    private ImageIcon img;
+
     
+
+    public ImageIcon getImg() {
+        return this.img;
+    }
+
+    public void setImg(ImageIcon img) {
+        this.img = img;
+    }
+
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDx() {
+        return this.dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public int getDy() {
+        return this.dy;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
+    public int getW() {
+        return this.w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return this.h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
 }
