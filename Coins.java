ceb2c52efce4b7ae210e32c390a228ba72c1562d
MarBomb8 <marlyssa.bombarde@yahoo.com>
2023-04-14 14:49:58 -0500

    get and set

--- Coins.java
+++ Coins.java
@@ -2,15 +2,15 @@ import javax.swing.ImageIcon;
 
 public class Coins {
     
-    private int x;
-    private int y;
-    private int dx;
-    private int dy;
-    private int w;
-    private int h;
-    private ImageIcon img;
-
-    public Coins() {
+   private int x;
+  private int y;
+  private int dx;
+  private int dy;
+  private int w;
+  private int h;
+  private ImageIcon img;
+  
+  public Coins() {
 		x= 1;
 		y = 1;
 		w = 1;
@@ -19,6 +19,64 @@ public class Coins {
 		dx = 0;
 		dy = 0;
 }
+
+  public int getX() {
+    return this.x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return this.y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public int getDx() {
+    return this.dx;
+  }
+
+  public void setDx(int dx) {
+    this.dx = dx;
+  }
+
+  public int getDy() {
+    return this.dy;
+  }
+
+  public void setDy(int dy) {
+    this.dy = dy;
+  }
+
+  public int getW() {
+    return this.w;
+  }
+
+  public void setW(int w) {
+    this.w = w;
+  }
+
+  public int getH() {
+    return this.h;
+  }
+
+  public void setH(int h) {
+    this.h = h;
+  }
+
+  public ImageIcon getImg() {
+    return this.img;
+  }
+
+  public void setImg(ImageIcon img) {
+    this.img = img;
+  }
+
+   
 
 
 
