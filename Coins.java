4ce82b63cf1e6627a69eb3b9bd3eb53e57c36918
dashmt21 <thurmd389@gmail.com>
2023-04-14 14:42:59 -0500

    pixart

--- Coins.java
+++ Coins.java
@@ -1,3 +1,30 @@
+import javax.swing.ImageIcon;
+
 public class Coins {
     
+    private int x;
+    private int y;
+    private int dx;
+    private int dy;
+    private int w;
+    private int h;
+    private ImageIcon img;
+
+    public Coins() {
+		x= 1;
+		y = 1;
+		w = 1;
+		h = 1;
+		img = new ImageIcon("");
+		dx = 0;
+		dy = 0;
+}
+
+
+
+
+
+
+
+
 }
