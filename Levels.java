b3a4456a7638c4a3adbfdb4dbc711eba163fc267
dashmt21 <thurmd389@gmail.com>
2023-04-20 16:01:42 -0500

    levels

--- Levels.java
+++ Levels.java
@@ -1,3 +1,16 @@
+import javax.swing.ImageIcon;
+
 public class Levels {
+    private ImageIcon bg;
+    private Long Time, startTime, startTime2, currentTime, lvlTimer;
+    private int deathcounter;
     
+    
+    public ImageIcon getBg() {
+        return bg;
+    }
+
+    public void setBg(ImageIcon bg) {
+        this.bg = bg;
+    }
 }
