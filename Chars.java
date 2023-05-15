f23cf06143d75ea646854101674b024a7b8378d3
MarBomb8 <marlyssa.bombarde@yahoo.com>
2023-05-15 15:55:55 -0500

    fixed collision!!!!!!

--- Chars.java
+++ Chars.java
@@ -1,3 +1,5 @@
+import java.awt.Rectangle;
+
 import javax.swing.ImageIcon;
 
 public class Chars {
@@ -94,6 +96,27 @@ public class Chars {
 
     public void setH(int h) {
         this.h = h;
+    }
+    public boolean collision(wbs wa){
+      Rectangle water= new Rectangle(wa.getX(), wa.getY(), wa.getW(), wa.getH());
+       Rectangle player= new Rectangle(x,y,w,h);
+
+       if(water.intersects(player)){
+       if(wa.getX() <= x+w && wa.getX()>=x){
+        setX(wa.getX()-1-w);
+    
+        return true;
+       }
+       else if(wa.getX() <=x){
+        setX(wa.getX()+wa.getW()+1);
+  
+
+         return true;
+       }
+
+
+    }
+       return false;
     }
 
 }
